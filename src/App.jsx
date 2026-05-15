2ec8cdeed05b6d652167c8f2e987792153b6a156
Your Full Name <your.email@example.com>
2026-05-15 14:43:11 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import Home from './pages/Home';
-import Categories from './pages/Categories';
-import AboutUs from './pages/AboutUs';
-import Contact from './pages/Contact';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Categories from "./pages/Categories";
+import AboutUs from "./pages/AboutUs";
+import Contact from "./pages/Contact";
 
-function App()
-{
-  return(
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Categories" element={<Categories />} />
-      <Route path="/AboutUs" element={<AboutUs />} />
-      <Route path="/Contact" element={<Contact />} />
-    </Routes>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
       <Footer />
     </BrowserRouter>
-    );
+  );
 }
+
 export default App;
